489b6fd61db45f902c81825d4f525b0aea640d6f
Džiugas Mažulis <dziugas.mazulis@gmail.com>
2019-09-25 01:09:16 +0300

    Update IntersectionFinder.cs
    *Fix intersection finder logic
    
    *convert indexes to zero-based

--- AnagramSolver/AnagramSolver.BusinessLogic/IntersectionFinder.cs
+++ AnagramSolver/AnagramSolver.BusinessLogic/IntersectionFinder.cs
@@ -17,27 +17,53 @@ namespace AnagramSolver.BusinessLogic
         public int[] FindEntries(DateTime fromDate, DateTime toDate)
         {
             int fromIndex = _entries.BinarySearch(new EntryModel { Date = fromDate });
-            fromIndex = ValidateIndex(fromIndex);
 
-            int toIndex = _entries.BinarySearch(new EntryModel { Date = toDate });
-            toIndex = ValidateIndex(toIndex) - 1;
+            EntryModel entryModel = new EntryModel { Date = toDate };
+            int startingIndex = fromIndex;
+            ValidateIndex(ref startingIndex);
+            int range = _entries.Count - startingIndex;
+            int toIndex = _entries.BinarySearch(startingIndex, range, entryModel, null);
 
-            if (fromIndex >= _entries.Count || toIndex < 0)
+
+            if (!DoEntriesIntersectWithDates(fromIndex, toIndex))
             {
                 return null;
             }
 
-            return new int[2] { fromIndex, toIndex };
+            if (IsEntryDateEqualToDate(fromIndex) && IsEntryDateEqualToDate(toIndex))
+            {
+                return new int[2] { fromIndex, toIndex};
+            }
+
+            if (IsEntryDateEqualToDate(fromIndex))
+            {
+                fromIndex++;
+            }
+
+            ValidateIndex(ref fromIndex);
+            ValidateIndex(ref toIndex);
+            ValidateIndexes(ref fromIndex, ref toIndex);
+
+            return new int[2] { fromIndex, toIndex};
         }
 
-        private int ValidateIndex(int index)
+        private bool DoEntriesIntersectWithDates(int fromIndex, int toIndex) => fromIndex == toIndex && fromIndex < 0 ? false : true;
+
+        private bool IsEntryDateEqualToDate(int index) => index >= 0 ? true : false;
+
+        private int ValidateIndex(ref int index) => index < 0 ? index = index * -1 - 1 : index;
+
+        private void ValidateIndexes(ref int fromIndex, ref int toIndex)
         {
-            if (index < 0)
+            if (fromIndex == -1)
             {
-                return index * -1 - 1;
+                fromIndex = 0;
             }
-            
-            return index;
+
+            if (toIndex == _entries.Count)
+            {
+                toIndex --;
+            }
         }
     }
 }
